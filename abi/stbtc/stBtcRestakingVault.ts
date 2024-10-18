ab66d1b6b76ac29760a9997335bdf61642276c2e
greatpie <greatpie@live.com>
2024-10-18 18:54:31 +0800

    upload abi

---
+++ abi/stbtc/stBtcRestakingVault.ts
@@ -0,0 +1,41 @@
+const stBTCRestakingVaultAbi = [
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    name: 'deposit',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    name: 'requestWithdraw',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256[]',
+        name: 'indexs',
+        type: 'uint256[]',
+      },
+    ],
+    name: 'claim',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+]
