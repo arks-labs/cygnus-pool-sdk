6ee2b11ca79e651222b04831d8289d8c68398256
wufeihuang <wufeihuang@foxmail.com>
2024-10-19 04:06:25 +0800

    docs: add doc

--- abi/stbtc/stBtcRestakingVault.ts
+++ abi/stbtc/stBtcRestakingVault.ts
@@ -38,4 +38,58 @@ const stBTCRestakingVaultAbi = [
     stateMutability: 'nonpayable',
     type: 'function',
   },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+    ],
+    name: 'getOrdersByOwner',
+    outputs: [
+      {
+        components: [
+          {
+            internalType: 'uint256',
+            name: 'index',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'amount',
+            type: 'uint256',
+          },
+          {
+            internalType: 'address',
+            name: 'owner',
+            type: 'address',
+          },
+          {
+            internalType: 'bool',
+            name: 'used',
+            type: 'bool',
+          },
+        ],
+        internalType: 'struct CygnusRestakingVault.WithdrawOrder[]',
+        name: '',
+        type: 'tuple[]',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'maxWithdrawIndex',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
 ]
